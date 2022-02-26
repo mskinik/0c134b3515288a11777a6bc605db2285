9a4e19e813dd26ad6ed84d586d4d771925690f22
Mustafa Süleyman Kınık <suleymankinik@outlook.com>
2022-02-27 02:29:01 +0300

    Created nav bar and nav host on main activity Created menu to navigate among fragments Created and configurated navigation component

--- app/src/main/java/com/mustafasuleymankinik/spacetraveler/view/MainActivity.kt
+++ app/src/main/java/com/mustafasuleymankinik/spacetraveler/view/MainActivity.kt
@@ -2,14 +2,30 @@ package com.mustafasuleymankinik.spacetraveler.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mustafasuleymankinik.spacetraveler.R
 import com.mustafasuleymankinik.spacetraveler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.navView
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
+
 }
